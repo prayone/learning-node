ed9e27f9536d3c9de3d3ab79cb61dd87c841379c
prayone <1054864949@qq.com>
2017-10-15 22:24:00 +0800

    kk

--- note.js
+++ note.js
@@ -1,3 +1,5 @@
+     babeljs.io可以将ES6代码转换为es5
+
 1.process.argv:属性返回一个数组，这个数组包含了启动Node.js进程时的命令行参数。
 				第一个元素为process.execPath。如果需要获取argv[0]的值请参见 process.argv0。第二个元素为当前执行的JavaScript文件路径。
 2.process.exit();终止程序进程
@@ -13,3 +15,9 @@
    	  		}
    	  })
 4.node中所有会发生阻塞的操作都是异步的（文件和网络）
+5.const 定义一个常量，值不可更改。
+      eg：<1>const a=10;     a=11;(会报错)  
+         <2>const b={a:10}   b.a=11;因为a是b的属性，所以不会报错
+  （1）一般用在引入模块上面,如果用var 就会有问题    var fs=require('fs')  fs=...;
+      const fs=require('fs')
+6.
